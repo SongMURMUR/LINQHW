3c3e33e1c01225a80c4135b733c51271179c8ca2
song <r8976a1670@gmail.com>
2024-09-18 13:32:58 +0800

    加入專案檔案。

---
+++ LinqLabs/作業/Frm作業_1.cs
@@ -0,0 +1,94 @@
+﻿using LinqLabs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHomeWork
+{
+    public partial class Frm作業_1 : Form
+    {
+        public Frm作業_1()
+        {
+            InitializeComponent();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //this.nwDataSet1.Products.Take(10);//Top 10 Skip(10)
+
+            //Distinct()
+        }
+
+        private void btnlog_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            var logFiles = from file in files
+                           where file.Extension.Equals(".log", StringComparison.OrdinalIgnoreCase) //StringComparison.OrdinalIgnoreCase 不分大小寫
+                           select file;
+
+
+            this.dataGridView1.DataSource = logFiles.ToList();
+
+        }
+
+        private void btn2022_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            DateTime thresholdDate = new DateTime(2022, 1, 1);
+
+            var oldFiles = from file in files
+                           where file.CreationTime < thresholdDate
+                           select file;
+
+            this.dataGridView1.DataSource = oldFiles.ToList();
+        }
+
+        private void btnlarge_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            var largeFiles = from file in files
+                             where file.Length > 10000
+                             select file;
+
+            this.dataGridView1.DataSource = largeFiles.ToList();
+        }
+
+        private void btnall_Click(object sender, EventArgs e)
+        {
+            this.ordersTableAdapter1.Fill(this.nwDataSet1.Orders);
+
+            var q = from o in this.nwDataSet1.Orders
+                    where !o.IsShippedDateNull()  //ShippedDate 有空值會爆錯 其他資料列也有空值 但可以從NullValue改成(Null)
+                    select o;
+                                                 
+
+            this.dataGridView1.DataSource = q.ToList();
+
+        }
+
+        private void btnxyear_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LinqLabs/作業/Frm作業_3.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace LinqLabs.作業
+{
+    partial class Frm作業_3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button36 = new System.Windows.Forms.Button();
+            this.button37 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button36
+            // 
+            this.button36.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button36.Location = new System.Drawing.Point(81, 54);
+            this.button36.Margin = new System.Windows.Forms.Padding(4);
+            this.button36.Name = "button36";
+            this.button36.Size = new System.Drawing.Size(332, 39);
+            this.button36.TabIndex = 143;
+            this.button36.Text = "搜尋 班級學生成績";
+            this.button36.UseVisualStyleBackColor = false;
+            this.button36.Click += new System.EventHandler(this.button36_Click);
+            // 
+            // button37
+            // 
+            this.button37.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button37.Location = new System.Drawing.Point(81, 152);
+            this.button37.Margin = new System.Windows.Forms.Padding(4);
+            this.button37.Name = "button37";
+            this.button37.Size = new System.Drawing.Size(332, 39);
+            this.button37.TabIndex = 142;
+            this.button37.Text = "每個學生個人成績";
+            this.button37.UseVisualStyleBackColor = false;
+            this.button37.Click += new System.EventHandler(this.button37_Click);
+            // 
+            // Frm作業_3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button36);
+            this.Controls.Add(this.button37);
+            this.Name = "Frm作業_3";
+            this.Text = "Frm作業_3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button36;
+        private System.Windows.Forms.Button button37;
+    }
+}
